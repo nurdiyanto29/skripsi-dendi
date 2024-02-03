ccf4faf5294a2acb660975f327e15f1ad4ee1198
xnr <nurdi014@gmail.com>
2024-02-03 20:54:21 +0700

    no message

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,8 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $fillable = [];
-
+    // use HasFactory;
+    protected $table = 'users';
+    protected $guarded = [];
     protected $hidden = [
         'password',
         'remember_token',
